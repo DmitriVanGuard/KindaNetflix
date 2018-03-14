7a5487cd710e1167d0330d03180d3560035d67a8
DmitriVanGuard <dimavanguard@gmail.com>
2018-03-14 11:44:21 +0200

    React router set up

---
+++ js/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Landing = () => (
+	<div className="landing">
+		<h1>KindaNetflix</h1>
+		<input type="text" placeholder="Search" />
+		<a href="a.html">or Browse All</a>
+	</div>
+);
+
+export default Landing;
