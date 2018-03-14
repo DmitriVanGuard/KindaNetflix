15db976d89366b8611f51082b9bd8eb901aab361
DmitriVanGuard <dimavanguard@gmail.com>
2018-03-14 13:47:31 +0200

    Movies data. Search page movies info. ShowCard component

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import preload from '../data.json';
+import ShowCard from './ShowCard';
 
-const Search = () => <h1>hlelo</h1>;
+const Search = () => (
+	<div className="search">{preload.shows.map(show => <ShowCard show={show} />)}</div>
+);
 
 export default Search;
