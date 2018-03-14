b48935a717fde07c8cfc7c20c200d46893df2c49
DmitriVanGuard <dimavanguard@gmail.com>
2018-03-14 20:14:27 +0200

    Search test. Dom event simulation tests

--- __tests__/Search.test.jsx
+++ __tests__/Search.test.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import preload from '../data.json';
 import Search from '../js/Search';
+import ShowCard from '../js/ShowCard';
 
 configure({ adapter: new Adapter() });
 
 test(`Search renders correctly`, () => {
 	const component = shallow(<Search />);
 	expect(component).toMatchSnapshot();
+});
+
+test(`Search should render correct amount of shows`, () => {
+	const component = shallow(<Search />);
+	expect(component.find(ShowCard).length).toEqual(preload.shows.length); // In toEqual what we expect it to be
+});
+
+test(`Search should render correct amount of shows based on search term`, () => {
+	const component = shallow(<Search />);
+	const searchWord = `black`;
+
+	component.find(`input`).simulate(`change`, { target: { value: searchWord } });
+	const showCount = preload.shows.filter(show =>
+		`${show.title} ${show.description}`.toUpperCase().includes(searchWord.toUpperCase())
+	).length;
+
+	expect(component.find(ShowCard).length).toEqual(showCount);
 });
