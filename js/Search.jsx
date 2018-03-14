45e0d453e2badeb7215e05866cbdb78e7287368d
DmitriVanGuard <dimavanguard@gmail.com>
2018-03-14 15:59:21 +0200

    Search component as a class in order to manipulate state

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import preload from '../data.json';
 import ShowCard from './ShowCard';
 
-const Search = () => (
-	<div className="search">
-		<div>
-			{preload.shows.map(show => (
-				<ShowCard key={show.imdbID} {...show /* anti patter. Better to be explicit */} />
-			))}
-		</div>
-	</div>
-);
+class Search extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			searchTerm: `this is some sort of debug statement`
+		};
+		this.handleSearchTermStateChange = this.handleSearchTermStateChange.bind(this);
+	}
+
+	handleSearchTermStateChange(evt) {
+		this.setState({ searchTerm: evt.target.value });
+	}
+
+	render() {
+		return (
+			<div className="search">
+				<header>
+					<h1>KindaNetflix</h1>
+					<input
+						type="text"
+						value={this.state.searchTerm}
+						onChange={this.handleSearchTermStateChange}
+						placeholder="Search"
+					/>
+				</header>
+				<div>
+					{preload.shows.map(show => (
+						<ShowCard key={show.imdbID} {...show /* anti patter. Better to be explicit */} />
+					))}
+				</div>
+			</div>
+		);
+	}
+}
 
 export default Search;
