0f2404f69770f19c5ec02838aa6fd88736a3378c
DmitriVanGuard <dimavanguard@gmail.com>
2018-03-15 22:40:04 +0200

    Landing class component. Form submit redirects to Search page

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,24 +1,41 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import type { RouterHistory } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { setSearchTermAC } from './actionCreators';
 
-const Landing = (props: { searchTerm: string, handleSearchTermStateChange: Function }) => (
-	<div className="landing">
-		<h1>KindaNetflix</h1>
-		<input
-			value={props.searchTerm}
-			type="text"
-			onChange={props.handleSearchTermStateChange} // handleSearchTermStateChange will be injected from mapDispatchToProps (line 27)
-			placeholder="Search"
-		/>
-		<Link href="/search" to="/search">
-			or Browse All
-		</Link>
-	</div>
-);
+type Props = {
+	searchTerm: string,
+	handleSearchTermStateChange: Function,
+	history: RouterHistory
+};
+class Landing extends Component<Props, any> {
+	goToSearch = (evt: SyntheticEvent<HTMLElement>) => {
+		evt.preventDefault();
+		this.props.history.push(`/search`); // Because Landing component is a `route` it has access to `history`
+	};
+
+	render() {
+		return (
+			<div className="landing">
+				<h1>KindaNetflix</h1>
+				<form onSubmit={this.goToSearch}>
+					<input
+						value={this.props.searchTerm}
+						type="text"
+						onChange={this.props.handleSearchTermStateChange} // handleSearchTermStateChange will be injected from mapDispatchToProps (line 27)
+						placeholder="Search"
+					/>
+				</form>
+				<Link href="/search" to="/search">
+					or Browse All
+				</Link>
+			</div>
+		);
+	}
+}
 
 const mapStateToProps = state => ({ searchTerm: state.searchTerm });
 /* This function will take only things from Redux store that Landing component cares about and put them into its props 
