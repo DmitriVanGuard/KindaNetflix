a7a30662d6f7ac600f1137aa8f42bb8319f2abb5
DmitriVanGuard <dimavanguard@gmail.com>
2018-03-15 10:42:41 +0200

    Details page

--- js/__tests__/Search.test.jsx
+++ js/__tests__/Search.test.jsx
@@ -1,3 +1,5 @@
+// @flow
+
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -8,17 +10,17 @@ import ShowCard from '../ShowCard';
 configure({ adapter: new Adapter() });
 
 test(`Search renders correctly`, () => {
-	const component = shallow(<Search />);
+	const component = shallow(<Search shows={preload.shows} />);
 	expect(component).toMatchSnapshot();
 });
 
 test(`Search should render correct amount of shows`, () => {
-	const component = shallow(<Search />);
+	const component = shallow(<Search shows={preload.shows} />);
 	expect(component.find(ShowCard).length).toEqual(preload.shows.length); // In toEqual what we expect it to be
 });
 
 test(`Search should render correct amount of shows based on search term`, () => {
-	const component = shallow(<Search />);
+	const component = shallow(<Search shows={preload.shows} />);
 	const searchWord = `black`;
 
 	component.find(`input`).simulate(`change`, { target: { value: searchWord } });
