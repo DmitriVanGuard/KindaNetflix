ad2ba0bf1bcae51ca42cf18ca582fcd1eb90e090
DmitriVanGuard <dimavanguard@gmail.com>
2018-03-15 11:27:54 +0200

    Details as Class Component. ratingsAPI express server

--- js/Details.jsx
+++ js/Details.jsx
@@ -1,29 +1,34 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 
-const Details = (props: { show: Show }) => {
-	const { title, description, year, poster, trailer } = props.show;
-	return (
-		<div className="details">
-			<Header />
-			<section>
-				<h1>{title}</h1>
-				<h2>({year})</h2>
-				<img src={`/public/img/posters/${poster}`} alt={`Poster for ${title}`} />
-				<p>{description}</p>
-			</section>
-			<div>
-				<iframe
-					src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&showinfo=0`}
-					frameBorder="0"
-					allowFullScrean
-					title={`Trailer for ${title}`}
-				/>
-			</div>
-		</div>
-	);
+type Props = {
+	show: Show
 };
+class Details extends Component<Props, {}> {
+	render() {
+		const { title, description, year, poster, trailer } = this.props.show;
+		return (
+			<div className="details">
+				<Header />
+				<section>
+					<h1>{title}</h1>
+					<h2>({year})</h2>
+					<img src={`/public/img/posters/${poster}`} alt={`Poster for ${title}`} />
+					<p>{description}</p>
+				</section>
+				<div>
+					<iframe
+						src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0&showinfo=0`}
+						frameBorder="0"
+						allowFullScrean
+						title={`Trailer for ${title}`}
+					/>
+				</div>
+			</div>
+		);
+	}
+}
 
 export default Details;
