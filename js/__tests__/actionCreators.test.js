451f2c02894d350f82996ff62f69bf0b6515192d
DmitriVanGuard <dimavanguard@gmail.com>
2018-03-16 13:30:45 +0200

    Testing async Thunk

--- js/__tests__/actionCreators.test.js
+++ js/__tests__/actionCreators.test.js
@@ -1,7 +1,19 @@
 // @flow
 /* eslint-disable quotes */
 
-import { setSearchTermAC, addAPIDataAC } from '../actionCreators';
+import moxios from 'moxios';
+import { setSearchTermAC, addAPIDataAC, getAPIDetailsTAC } from '../actionCreators';
+
+const billions = {
+	title: 'Billions',
+	year: '2016–',
+	description:
+		'U.S. Attorney Chuck Rhoades goes after hedge fund king, Bobby "Axe" Axelrod in a battle between two powerful New York figures.',
+	poster: 'b.jpg',
+	imdbID: 'tt4270492',
+	trailer: '_raEUMLL-ZI',
+	rating: '8.6'
+};
 
 // Action creators are tested with snapshots
 test(`setSearchTermAC`, () => {
@@ -9,16 +21,28 @@ test(`setSearchTermAC`, () => {
 });
 
 test(`addAPIDataAC`, () => {
-	expect(
-		addAPIDataAC({
-			title: 'Billions',
-			year: '2016–',
-			description:
-				'U.S. Attorney Chuck Rhoades goes after hedge fund king, Bobby "Axe" Axelrod in a battle between two powerful New York figures.',
-			poster: 'b.jpg',
-			imdbID: 'tt4270492',
-			trailer: '_raEUMLL-ZI',
-			rating: '8.6'
-		})
-	).toMatchSnapshot(); // Because addAPIDataAC will return JSON structure
+	expect(addAPIDataAC(billions)).toMatchSnapshot(); // Because addAPIDataAC will return JSON structure
+});
+
+test('getAPIDetailsTAC', (done: Function /* becouse it is async */) => {
+	const dispatchMock = jest.fn(); // spy function
+	moxios.withMock(() => {
+		// If JS will ever call axios, moxios will subtitute it withMock
+		const thunk = getAPIDetailsTAC(billions.imdbID);
+		thunk(dispatchMock);
+
+		moxios.wait(() => {
+			const request = moxios.requests.mostRecent(); // Did our thunk reach out to correct api
+			request
+				.respondWith({
+					status: 200,
+					response: billions
+				})
+				.then(() => {
+					expect(request.url).toEqual(`http://localhost:3000/${billions.imdbID}`);
+					expect(dispatchMock).toBeCalledWith(addAPIDataAC(billions)); // We must test addAPIDataAC before thunk calls, cos its depending on it
+					done();
+				});
+		});
+	});
 });
