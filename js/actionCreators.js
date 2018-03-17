cb3ea24c78debec5de3cf4e001077ecd76e40e64
DmitriVanGuard <dimavanguard@gmail.com>
2018-03-17 23:07:40 +0200

    Working async/await

--- js/actionCreators.js
+++ js/actionCreators.js
@@ -14,12 +14,12 @@ export function addAPIDataAC(apiData: Show) {
 export function getAPIDetailsTAC(imdbID: string) {
 	// Handle thunk async request
 	// Normal actionCreators return an object, Thunk actionCreator will return a function - thunk
-	return (dispatch: Function) => {
-		axios
-			.get(`http://localhost:3000/${imdbID}`)
-			.then(response => {
-				dispatch(addAPIDataAC(response.data)); // Dispatching new action made by addAPIDataAC to rootReducer.
-			})
-			.catch(error => console.error(`API request error ->`, error)); // eslint-disable-line no-console
+	return async (dispatch: Function) => {
+		try {
+			const response = await axios.get(`http://localhost:3000/${imdbID}`);
+			dispatch(addAPIDataAC(response.data)); // Dispatching new action made by addAPIDataAC to rootReducer.
+		} catch (err) {
+			console.error(`Fetching rating error ->`, err); // eslint-disable-line
+		}
 	};
 }
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
 		'react-hot-loader/patch',
 		'webpack-dev-server/client?http://localhost:8080',
 		'webpack/hot/only-dev-server',
+		'regenerator-runtime/runtime.js',
 		'./js/ClientApp.jsx'
 	],
 	devtool: 'cheap-eval-source-map',
